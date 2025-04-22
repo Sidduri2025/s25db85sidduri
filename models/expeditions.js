f1452b7ad249902eac72a27db4f4cf401a6f494c
Sidduri2025 <vaishusidduri@gmail.com>
2025-04-22 17:19:27 -0500

    modified

--- models/expeditions.js
+++ models/expeditions.js
@@ -1,8 +1,23 @@
 const mongoose = require("mongoose")
 const expeditionsSchema = mongoose.Schema({
-    expeditions_type: String,
-    size: String,
-    cost: Number
+    expeditions_type: {
+        type: String,
+        required: [true, "Expeditions type is required"],
+        minlength: [3, "type must be At least 3"],
+    },
+
+    size: {
+        type: String,
+        required: [true, "Expeditions size is required"],
+        minlength: [2, "type must be At least 2"],
+
+    },
+    cost: {
+        type: Number,
+        required: [true, "Expeditions cost is required"],
+        min: [3, "type must be At least 3"],
+    },
+
 })
 module.exports = mongoose.model("expeditions",
     expeditionsSchema)
--- routes/expeditions.js
+++ routes/expeditions.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET expeditions listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   let expeditions = [
     { name: "Everest Base Camp", destination: "Nepal", duration: 14 },
     { name: "Amazon Rainforest", destination: "Brazil", duration: 10 },
@@ -11,10 +11,19 @@ router.get('/', function(req, res, next) {
   res.render('expeditions', { title: "Search Results - Expeditions", expeditions });
 });
 var express = require('express');
-const expeditions_controlers= require('../controllers/expeditions');
+const expeditions_controlers = require('../controllers/expeditions');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on
+// or
+// redirect to login.
+const secured = (req, res, next) => {
+  if (req.user) {
+    return next();
+  }
+  res.redirect("/login");
+}
 /* GET costumes */
-router.get('/', expeditions_controlers.expeditions_view_all_Page );
+router.get('/', expeditions_controlers.expeditions_view_all_Page);
 
 /* GET detail costume page */
 router.get('/detail', expeditions_controlers.expeditions_view_one_Page);
@@ -22,11 +31,13 @@ router.get('/detail', expeditions_controlers.expeditions_view_one_Page);
 /* GET create costume page */
 router.get('/create', expeditions_controlers.expeditions_create_Page);
 
-/* GET create update page */
-router.get('/update', expeditions_controlers.expeditions_update_Page);
+
 
 /* GET delete costume page */
 router.get('/delete', expeditions_controlers.expeditions_delete_Page);
+/* GET update costume page */
+router.get('/update', secured,expeditions_controlers.expeditions_update_Page);
+
 
 
 
